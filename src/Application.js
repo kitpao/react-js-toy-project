2b06e56f8442f8cbaa590638d846f381d5818bda
Kitpao <kitpao91@gmail.com>
2021-10-06 22:50:00 -0500

    Add application
    using render with events on state change

---
+++ src/Application.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+
+class Application extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 0,
+      overTen: false,
+    }
+  }
+
+  //UNSAFE_componentWillMount(props, state){}
+
+  //componentDidMount(props, state){}
+
+  //UNSAFE_componentWillReceiveProps(props){  //detects state change, before it mounts}
+
+  //UNSAFE_componentWillUpdate(props, state){ //change in properties: this.props(current), props(old)
+    //if (this.props.name !== props.name){
+      // do something
+    //}
+  //}
+
+  componentDidUpdate(props, state){ //when rerender happens: this.props(current), props(old)
+    // console.log("Updated from", state, "to", this.state )
+    if(this.state.count > 10 && this.state.count !== state.count && !this.state.overTen){
+      // only when its bigger, when it is called for the first time (avoid infinite loop), when flag changed
+      console.log("Updating over ten");
+      this.setState({overTen: true});
+    }
+  }
+  
+  handleClick = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  render() {
+      let {count} = this.state;
+      let name = "Kit";
+    return (
+      <div>
+        <h1>Hello, {name}</h1>
+        <h2>You clicked the button {count} times</h2>
+        {(this.state.overTen)
+          ? <h3>Beat high score of 10!</h3>
+          : null
+        }
+        
+        <span>
+          <button onClick={(e)=> this.handleClick()}>CLICK ME</button>  
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Application;
